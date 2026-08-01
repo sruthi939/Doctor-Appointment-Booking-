357681812d232eed2696dfa754d05a0c1745f770
sruthi939 <sathiyasuru@gmail.com>
2026-08-01 15:50:32 +0530

    implement home page with speciality menu and top doctors components using Tailwind CSS

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -9,7 +9,11 @@ const SpecialityMenu = () => {
             <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
             <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
                 {specialityData.map((item, index) > (
-                    <Link to={`/doctors/${item.speciality}`}>
+                    <Link
+                        onClick={() => scrollTo(0, 0)}
+                        className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500'
+                        to={`/doctors/${item.speciality}`}
+                    >
                         <img
                             className='w-16 sm:w-24 mb-2'
                             src={item.image}
