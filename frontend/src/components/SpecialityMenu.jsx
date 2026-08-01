f52f6308fecf67792d7d09387fb659bbb53af2cd
sruthi939 <sathiyasuru@gmail.com>
2026-08-01 10:09:46 +0530

    add SpecialityMenu component and global Tailwind CSS configurations

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom';
 
 const SpecialityMenu = () => {
     return (
-        <div id='speciality'>
-            <h1></h1>
-            <p></p>
-            <div>
-                {specialityData.map((item) > (
-                    <Link>
-
+        <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
+            <h1 className='text-3xl font-medium'>Find the Speciality</h1>
+            <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+            <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
+                {specialityData.map((item, index) > (
+                    <Link to={`/doctors/${item.speciality}`}>
+                        <img
+                            className='w-16 sm:w-24 mb-2'
+                            src={item.image}
+                            alt=''
+                        />
+                        <p>{item.speciality}</p>
                     </Link>
                 ))}
             </div>
