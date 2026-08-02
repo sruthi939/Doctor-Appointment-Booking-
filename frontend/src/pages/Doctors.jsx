3e917d5c7f9e9b9d635b1bbdecd91446f49e6aa4
sruthi939 <sathiyasuru@gmail.com>
2026-08-02 20:06:02 +0530

    add Doctors page component and initialize global CSS with Tailwind configuration

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppConext } from '../context/AppContext'
 
 const Doctors = () => {
+
+    const { speciality } = useParams();
+    const [filterDoc, setFilterDoc] = useState([]);
+
+    const [doctors] = useContext(AppConext);
+
     return (
-        <div>Doctors</div>
+        <div>
+            <p>Browser through the doctors specialist.</p>
+            <div>
+                <div>
+                    <p>Cardiologist</p>
+                    <p>Neurologist</p>
+                    <p>Orthopedics</p>
+                    <p>Pediatristion</p>
+                    <p>Dermatologist</p>
+                    <p>Gynecologist</p>
+                    <p>ENT</p>
+                    <p>Ophthalmologist</p>
+                    <p>Dentist</p>
+                    <p>Pulmonologist</p>
+                    <p>Urologist</p>
+                    <p>Oncologist</p>
+                    <p>General Physician</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
