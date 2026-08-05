872c17e25f4365f4e07bcf19a6eceba7205fe97e
sruthi939 <sathiyasuru@gmail.com>
2026-08-05 15:22:05 +0530

    add MyProfile page with user data state and edit toggle functionality

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+
+    const [useData, setUserData] = useState({
+        name: "",
+        image: assets.profile_pic,
+        email: "",
+        phone: "",
+        address: {
+            line1: "",
+            line2: ""
+        },
+        gender: "",
+        dob: ""
+    })
+
+    const [IsEdit, setIsEdit] = useState(true)
+
     return (
-        <div>MyProfile</div>
+        <div>
+            <img src={useData.image} alt='' />
+            {
+                IsEdit
+                    ? <input
+                        type='text'
+                        value={setUserData.name}
+                        onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))}
+                    />
+                    : <p>{setUserData.name}</p>
+            }
+            <hr />
+            <div>
+                <p>CONTACT INFORMATION</p>
+                <div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
