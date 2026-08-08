fdd8172bfe99544ee5d418286ca91745aa4ec1c3
sruthi939 <sathiyasuru@gmail.com>
2026-08-08 15:22:00 +0530

    initialize backend server with express and route structure

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv/config';
 import connectDB from './config/db.js';
 
 import authRoutes from './routes/auth.routes.js';
