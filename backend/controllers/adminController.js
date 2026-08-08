785c6b80b5da05adfd744c32e9c68588c7013562
sruthi939 <sathiyasuru@gmail.com>
2026-08-08 19:02:45 +0530

    initialize backend server and implement admin doctor addition functionality

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -8,6 +8,7 @@ const addDoctor = async (req, res) => {
 
     try {
         const { name, email, password, speciality, degree, experience, about, fees, address } = req.body
+        const imageFile = req.file
     } catch (error) {
 
     }
