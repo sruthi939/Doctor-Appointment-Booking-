d90b12bcd5ec7db87b532abcc174764d86abcb5d
sruthi939 <sathiyasuru@gmail.com>
2026-08-08 16:20:24 +0530

    initialize backend server with MongoDB and Cloudinary configurations

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js';
 
 // app config
 const app = express();
 const port = process.env.PORT || 5000
+connectDB()
 
 // middlewares
 app.use(express.json())
