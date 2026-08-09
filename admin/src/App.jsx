11a3efba6af40b95e1d06378e2adc4ae24fd7e9b
sruthi939 <sathiyasuru@gmail.com>
2026-08-10 00:56:28 +0530

    implement responsive sidebar navigation for admin and doctor panels

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,24 @@
 import React, { useContext } from 'react'
 import Login from './pages/Login.jsx';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from './context/AdminContext.jsx';
 import Navbar from './components/Navbar.jsx';
+import Sidebar from './components/Sidebar.jsx';
 
 const App = () => {
-
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div className='bg-[#F8F9FD]'>
+    <div className='bg-[#F8F9FD] min-h-screen'>
       <ToastContainer />
       <Navbar />
+      <div className='flex items-start'>
+        <Sidebar />
+        <div className='flex-1 p-6'>
+          {/* Routes render here */}
+        </div>
+      </div>
     </div>
   ) : (
     <>
